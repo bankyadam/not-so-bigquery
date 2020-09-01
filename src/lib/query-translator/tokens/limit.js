76961f811da7d7da85ca1b5a63285b02f2e5ed97
Adam Bánky <banky.adam@gmail.com>
2020-09-01 23:21:56 +0200

    refact(query-translator): simplify token declarations

--- src/lib/query-translator/tokens/limit.js
+++ src/lib/query-translator/tokens/limit.js
@@ -1,12 +1,8 @@
 'use strict';
 
 const { createToken } = require('chevrotain');
-const Identifier = require('./identifier');
-const AnyWord = require('./anyword');
 
 module.exports = createToken({
   name: 'Limit',
-  pattern: /LIMIT (0|[1-9]\d*)/i,
-  longer_alt: Identifier,
-  categories: [AnyWord]
+  pattern: /LIMIT (0|[1-9]\d*)/i
 });
--- src/lib/query-translator/tokens/offset.js
+++ src/lib/query-translator/tokens/offset.js
@@ -1,12 +1,8 @@
 'use strict';
 
 const { createToken } = require('chevrotain');
-const Identifier = require('./identifier');
-const AnyWord = require('./anyword');
 
 module.exports = createToken({
   name: 'Offset',
-  pattern: /OFFSET (0|[1-9]\d*)/i,
-  longer_alt: Identifier,
-  categories: [AnyWord]
+  pattern: /OFFSET (0|[1-9]\d*)/i
 });
