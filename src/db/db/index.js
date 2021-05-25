af377f11f9a44dcda257ae46416f0bc4c390d10c
Adam Bánky <banky.adam@gmail.com>
2021-05-25 11:19:20 +0200

    feat(db): add retry on connecting to db

--- src/db/db/index.js
+++ src/db/db/index.js
@@ -2,11 +2,35 @@
 
 const { Client } = require('pg');
 const { POSTGRES_TYPES } = require('../bigQuery/types');
+const RETRY_TIME = 1500;
+const MAX_RETRY_COUNT = 5;
 
 module.exports = class Db {
   constructor(connectionString) {
-    this._db = new Client({ connectionString });
-    this._db.connect();
+    this._connectionString = connectionString;
+    this._retryCount = 0;
+    this._connect();
+  }
+
+  _connect() {
+    this._db = new Client({ connectionString: this._connectionString });
+    this._db.connect()
+      .then(this._onConnectionSuccess.bind(this))
+      .catch(this._onConnectionFail.bind(this));
+  }
+
+  _onConnectionSuccess() {
+    this._retryCount = 0;
+    console.info('Connection to DB was successful');
+  }
+
+  _onConnectionFail(e) {
+    if (this._retryCount++ < MAX_RETRY_COUNT) {
+      setTimeout(this._connect.bind(this), RETRY_TIME);
+      return;
+    }
+
+    throw e;
   }
 
   async query(query, values) {
@@ -15,12 +39,12 @@ module.exports = class Db {
 
   async tableExists(schemaName, tableName) {
     const result = await this._db.query(`
-        SELECT EXISTS(
-                       SELECT
-                       FROM information_schema.TABLES
-                       WHERE TABLE_SCHEMA = $1
-                         AND TABLE_NAME = $2
-                   )
+        SELECT EXISTS (
+            SELECT
+            FROM information_schema.TABLES 
+            WHERE TABLE_SCHEMA = $1
+              AND TABLE_NAME = $2
+        )
     `, [schemaName, tableName]);
     return result.rows.length > 0 && result.rows[0].exists;
   }
