efaf5764f8a69b1dd2d7eac72f241cabbd61ffa6
Banky Adam <banky.adam@gmail.com>
2021-05-26 11:26:32 +0200

    refact(query-translator): reorganize tests and testcases

--- test/mocha-init.js
+++ test/mocha-init.js
@@ -1,10 +1,15 @@
 'use strict';
+const fs = require('fs');
 
 const sinon = global.sinon = require('sinon');
 const chai = require('chai');
 global.expect = chai.expect;
 chai.use(require('sinon-chai'));
 chai.use(require('chai-string'));
+
+require.extensions['.txt'] = function(module, filename) {
+  module.exports = fs.readFileSync(filename, 'utf8');
+};
 
 beforeEach(function() {
   global.sandbox = sinon.createSandbox();
---
+++ test/unit/query-translator/run-expectation.js
@@ -0,0 +1,15 @@
+'use strict';
+const subject = require('../../../src/lib/query-translator');
+
+const _getTestCaseData = function(content) {
+  return /^--INPUT--\n([\s\S]+?)\n--EXPECT--\n([\s\S]*?)$/.exec(content);
+};
+
+module.exports = function(content) {
+  return () => {
+    const result = _getTestCaseData(content);
+    expect(subject(result[1], 'defaultProject').toLowerCase())
+      .to.be.equalIgnoreSpaces(result[2].toLowerCase());
+  };
+};
+
