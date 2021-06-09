c8ad60964ae2a438e7434d69d7e201abb462e883
Banky Adam <banky.adam@gmail.com>
2021-06-10 00:45:40 +0200

    test(query-translator): test lexer token definition

---
+++ test/unit/query-translator/lexer.spec.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { mapKeys } = require('lodash');
+
+const Lexer = require('../../../src/lib/query-translator/lexer');
+const TOKENS = require('../../../src/lib/query-translator/tokens');
+
+describe('Lexer Definition', function() {
+  it('has length of token\'s count', function() {
+    expect(Lexer.lexerDefinition).to.have.lengthOf(Object.keys(TOKENS).length);
+  });
+
+  mapKeys(TOKENS, function(token, tokenName) {
+    it('contains token: ' + tokenName, function() {
+      expect(Lexer.lexerDefinition).contain(token);
+    });
+  });
+});
