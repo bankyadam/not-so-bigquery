5572b41d23e411c198db4ed417ec07b633d46099
Banky Adam <banky.adam@gmail.com>
2021-07-30 22:45:46 +0200

    refactor(e2e): speeding up setup of e2e test run

--- test/e2e/query-support/runTestCase.js
+++ test/e2e/query-support/runTestCase.js
@@ -1,9 +1,14 @@
 'use strict';
 
+require.extensions['.txt'] = function(module, filename) {
+  module.exports = fs.readFileSync(filename, 'utf8');
+};
+
+const fs = require('fs');
 const { mapValues } = require('lodash');
 
-const bq = require('../common/connection-fake');
-const convertTable = require('../../support/table-to-json');
+const bq = require('../support/connection-fake');
+const convertTable = require('../../unit/support/table-to-json');
 const prepareTable = function(tableData) {
   return tableData
     .replace(/{CURRENT_DATE}/g, (new Date).toISOString().substr(0, 10));
@@ -24,7 +29,8 @@ const NOT_ORDERED = function(currentData, expectedData) {
 module.exports = function(content, expectation) {
   expectation = expectation || DEFAULT;
   return async () => {
-    const result = _getTestCaseData(content);
+    // eslint-disable-next-line security/detect-non-literal-require
+    const result = _getTestCaseData(require(content));
     let [data] = await bq.query(result[1]);
     data = data.map(row => {
       return mapValues(row, value => {
