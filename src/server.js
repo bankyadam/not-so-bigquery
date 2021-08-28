5dec12a2c38a3de48d5015dbd75fe8594e068f41
Banky Adam <banky.adam@gmail.com>
2021-08-28 21:03:08 +0200

    feat(server): remove HTTPS, since we only use this to development

--- src/server.js
+++ src/server.js
@@ -1,19 +1,11 @@
 'use strict';
 
-const fs = require('fs');
-const https = require('https');
+const http = require('http');
 const app = require('./application');
 
-const getCertificate = function() {
-  const key = fs.readFileSync(__dirname + '/../docker/app/cert/key.pem');
-  const cert = fs.readFileSync(__dirname + '/../docker/app/cert/cert.pem');
-
-  return { key, cert };
-};
-
 module.exports = function(port, hostname) {
-  const server = https.createServer(Object.assign({}, getCertificate()), app);
+  const server = http.createServer(app);
   server.listen(port, hostname, () => {
-    console.log(`App listening at https://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
   });
 };
